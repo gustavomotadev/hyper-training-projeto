5ba7cfb42513de05da6194a72b36c36668f3d4da
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-21 19:59:54 -0300

    Criação da Classe Escala e seu repositório incompleto.

---
+++ TechBeauty/TechBeauty.Dominio/Repositorio/EscalaRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Dominio.Repositorio
+{
+    public class EscalaRepositorio
+    {
+        public List<Escala> TabelaEscala { get; set; } = new List<Escala>;
+
+        public EscalaRepositorio()
+        {
+            Preencher();
+        }
+
+        public void Incluir(Escala escala) => TabelaEscala.Add(escala);
+
+        public Escala SelecionarPorId(int id) => TabelaEscala.FirstOrDefault(x => x.Id == id);
+
+        public void Alterar(int id, DateTime dataHoraEntrada, DateTime dataHoraSaida,
+            Colaborador colaborador) => SelecionarPorId(id).Alterar(dataHoraEntrada, dataHoraSaida, colaborador);
+
+        public void Excluir(int id) => TabelaEscala.Remove(SelecionarPorId(id));
+
+        public void Preencher()
+        {
+
+        }
+    }
+}
