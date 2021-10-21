afb55d37478ce6f7c81787cbc01fc273a14daf08
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-21 09:24:31 -0300

    Commit inicial criação da Classe OSRepositorio.

---
+++ TechBeauty/TechBeauty.Dominio/Repositorio/OSRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBeauty.Dominio.Repositorio
+{
+    public class OSRepositorio
+    {
+    }
+}
