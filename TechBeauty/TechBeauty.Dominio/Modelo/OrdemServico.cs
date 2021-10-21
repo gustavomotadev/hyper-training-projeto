44ca1262aeade81ee1d743d7c9f3a477db05b362
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-21 09:30:37 -0300

    CRUD da classe OrdemServico.

--- TechBeauty/TechBeauty.Dominio/Modelo/OrdemServico.cs
+++ TechBeauty/TechBeauty.Dominio/Modelo/OrdemServico.cs
@@ -4,10 +4,33 @@ namespace TechBeauty.Dominio.Modelo
 {
     public class OrdemServico
     {
-        public int Id { get; set; }
-        public decimal PrecoTotal { get; set; }
-        public int DuracaoTotal { get; set; }
-        public Cliente Cliente { get; set; }
-        public StatusOS StatusOS { get; set; }
+        public int Id { get; private set; }
+        public decimal PrecoTotal { get; private set; }
+        public int DuracaoTotal { get; private set; }
+        public Cliente Cliente { get; private set; }
+        
+        //Não sei se cabe o private já que o enum é fixo.
+        public StatusOS StatusOS { get; private set; }
+
+        public static OrdemServico Criar(int id, decimal precoTotal, 
+            int duracaoTotal, Cliente cliente, StatusOS statusOs)
+        {
+            var os = new OrdemServico();
+            os.Id = id;
+            os.PrecoTotal = precoTotal;
+            os.DuracaoTotal = duracaoTotal;
+            os.Cliente = cliente;
+            os.StatusOS = statusOs;
+            return os;
+        }
+
+        public void Alterar(decimal precoTotal,
+            int duracaoTotal, Cliente cliente, StatusOS statusOs)
+        {
+            PrecoTotal = precoTotal;
+            DuracaoTotal = duracaoTotal;
+            Cliente = cliente;
+            StatusOS = statusOs;
+        }
     }
 }
