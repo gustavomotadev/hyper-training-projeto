f85694deeaa0507ae25d104db8b069cf09026d3b
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-23 11:50:40 -0300

    Modificando propriedades que não se alteram e colocando-as no construtor.

--- TechBeauty/TechBeauty.Dominio/Modelo/ContratoTrabalho.cs
+++ TechBeauty/TechBeauty.Dominio/Modelo/ContratoTrabalho.cs
@@ -6,25 +6,24 @@ namespace TechBeauty.Dominio.Modelo
     public class ContratoTrabalho
     {
         public int Id { get; init; }
-        public RegimeContratual RegimeContratual { get; private set; }
-        public DateTime DataEntrada { get; private set; }
+        public RegimeContratual RegimeContratual { get; init; }
+        public DateTime DataEntrada { get; init; }
         public DateTime? DataDesligamento { get; private set; }
         public List<Cargo> Cargos { get; private set; }
-        public string CnpjCTPS { get; private set; }
+        public string CnpjCTPS { get; init; }
 
-        public ContratoTrabalho(int id)
+        public ContratoTrabalho(int id, RegimeContratual regimeContratual, DateTime dataEntrada)
         {
             Id = id;
+            RegimeContratual = regimeContratual;
+            DataEntrada = dataEntrada;
         }
         public static ContratoTrabalho NovoContratoTrabalho(int idContratoTrabalho, RegimeContratual regimeContratual, DateTime dataEntrada,
             DateTime? dataDesligamento, List<Cargo> cargos, string cnpjCTPS)
         {
-            var contratoTrabalho = new ContratoTrabalho(idContratoTrabalho);
-            contratoTrabalho.RegimeContratual = regimeContratual;
-            contratoTrabalho.DataEntrada = dataEntrada;
+            var contratoTrabalho = new ContratoTrabalho(idContratoTrabalho, regimeContratual, dataEntrada)
             contratoTrabalho.DataDesligamento = dataDesligamento;
-            contratoTrabalho.Cargos = cargos;
-            contratoTrabalho.CnpjCTPS = cnpjCTPS;
+            contratoTrabalho.Cargos = cargos
             return contratoTrabalho;
         }
         public void Alterar(RegimeContratual regimeContratual, DateTime dataEntrada,
