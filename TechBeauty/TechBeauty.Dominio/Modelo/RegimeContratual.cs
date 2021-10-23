2fa7ec4bf6cdddf7ad94147cd2d9afa4089bf01f
gsimoes00 <gustavomota001@gmail.com>
2021-10-23 17:31:12 -0300

    Construtor privado em RegimeContratual

--- TechBeauty/TechBeauty.Dominio/Modelo/RegimeContratual.cs
+++ TechBeauty/TechBeauty.Dominio/Modelo/RegimeContratual.cs
@@ -5,16 +5,14 @@
         public int Id { get; init; }
         public string TipoRegimeContratual { get; init; }
 
-        public RegimeContratual(int id, string tipoRegimeContratual)
+        private RegimeContratual(int id, string tipoRegimeContratual)
         {
             Id = id;
             TipoRegimeContratual = tipoRegimeContratual;
         }
-        public static RegimeContratual AdicionarRegimeContratual(int idRegimeContratual, string tipoRegimeContratual)
-        {
-            var regimeContratual = new RegimeContratual(idRegimeContratual, tipoRegimeContratual);
-            return regimeContratual;
-        }
+
+        public static RegimeContratual AdicionarRegimeContratual(int idRegimeContratual, string tipoRegimeContratual) => 
+            new RegimeContratual(idRegimeContratual, tipoRegimeContratual);
 
     }
 }
