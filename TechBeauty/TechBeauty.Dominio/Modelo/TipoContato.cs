0e325ac56f1cc433e36c78160a6052e5aec6dec2
gsimoes00 <gustavomota001@gmail.com>
2021-10-23 17:34:58 -0300

    Construtor privado em TipoContato

--- TechBeauty/TechBeauty.Dominio/Modelo/TipoContato.cs
+++ TechBeauty/TechBeauty.Dominio/Modelo/TipoContato.cs
@@ -5,18 +5,14 @@
         public int Id { get; init; }
         public string Valor { get; init; }
 
-        public TipoContato(int id, string valor)
+        private TipoContato(int id, string valor)
         {
             Id = id;
             Valor = valor;
         }
 
-        public static TipoContato AdicionarTipoContato(int idTipoContato, string valor)
-        {
-            var tipoContato = new TipoContato(idTipoContato, valor);
-            
-            return tipoContato;
-        }
+        public static TipoContato NovoTipoContato(int idTipoContato, string valor) => 
+            new TipoContato(idTipoContato, valor);
         
     }
 }
