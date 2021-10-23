dffc7b14fa617f0a128dfacc09fbb3086b85b674
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-23 11:44:15 -0300

    Aprimorando a Classe Endereço e adicionando os métodos alterar específicos.

--- TechBeauty/TechBeauty.Dominio/Modelo/Endereco.cs
+++ TechBeauty/TechBeauty.Dominio/Modelo/Endereco.cs
@@ -4,33 +4,65 @@
     {
         public int Id { get; init; }
         public string Logradouro { get; private set; }
+        public string Bairro { get; set; }
         public string Cidade { get; private set; }
         public string UF { get; private set; }
-        public string Numero { get; private set; }
-        public string Complemento { get; private set; }
+        public string Numero { get; private set; } = "s/n";
+        public string Complemento { get; private set; } = string.Empty;
+        public string CEP { get; set; }
 
-        public Endereco(int id )
+        public Endereco(int id)
         {
             Id = id;
         }
-        public static Endereco NovoEndereco(int idEndereco, string logradouro, string cidade, string uf, string numero, string complemento)
+        public static Endereco NovoEndereco(int idEndereco, string logradouro, string numero, string bairro,
+            string cidade, string uf, string cep, string complemento)
         {
             var endereco = new Endereco(idEndereco);
             endereco.Logradouro = logradouro;
+            endereco.Numero = numero;
+            endereco.Bairro = bairro;
             endereco.Cidade = cidade;
             endereco.UF = uf;
-            endereco.Numero = numero;
+            endereco.CEP = cep;
             endereco.Complemento = complemento;
             return endereco;
         }
 
-        public void Alterar(string logradouro, string cidade, string uf, string numero, string complemento)
+        public void Alterar(string logradouro, string numero, string bairro,
+            string cidade, string uf, string cep, string complemento)
         {
             Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
             Cidade = cidade;
             UF = uf;
-            Numero = numero;
+            CEP = cep;
             Complemento = complemento;
+        }
+
+        public void AlterarCidade(string logradouro, string numero, string bairro,
+            string cidade, string complemento)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Complemento = complemento;
+        }
+
+        public void AlterarLogradouro(string logradouro, string numero, string bairro,
+            string complemento)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Complemento = complemento;
+        }
+
+        public void AlterarNumero(string numero)
+        {
+            Numero = numero;   
         }
     }
 }
