34937ca9013f5722eb70424d1967d00873fefdf9
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-26 10:19:13 -0300

    Criando enum Lista de UF para a Classe Endereço.

---
+++ TechBeauty/TechBeauty.Dominio/Modelo/Enumeradores/ListaUF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBeauty.Dominio.Modelo.Enumeradores
+{
+    public enum ListaUF
+    {
+        AC,
+        AL,
+        AP,
+        AM,
+        BA,
+        CE,
+        DF,
+        ES,
+        GO,
+        MA,
+        MT,
+        MS,
+        MG,
+        PA,
+        PB,
+        PR,
+        PE,
+        PI,
+        RJ,
+        RN,
+        RS,
+        RO,
+        RR,
+        SC,
+        SP,
+        SE,
+        TO
+    }
+}
