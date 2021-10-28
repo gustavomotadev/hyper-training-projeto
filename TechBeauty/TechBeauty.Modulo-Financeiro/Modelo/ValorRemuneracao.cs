6477c9b53fc3a1ff88d7cb8a72622436868ffa5f
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-28 20:13:24 -0300

    Criação da Classe ValorRemuneracao incompleta.

---
+++ TechBeauty/TechBeauty.Modulo-Financeiro/Modelo/ValorRemuneracao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Modulo_Financeiro.Modelo
+{
+    public class ValorRemuneracao
+    {
+        public static decimal SalarioMinino { get; private set; }
+        public Colaborador Colaborador { get; private set; }
+        public DateTime HorasTrabalhadas { get; private set; }
+        public decimal SalarioHora { get; private set; }
+        public decimal Comissao { get; private set; }
+        public decimal AdicionalNoturno { get; private set; }
+        public decimal AdicionalHrExtra { get; private set; }
+
+
+
+    }
+}
