c6fb19cd930269bafc407366ccb2170ec1383fc4
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-10-28 20:07:33 -0300

    Criação do Módulo Financeiro, referência na Program, criação das classes FormaPagamento e Pagamento.

---
+++ TechBeauty/TechBeauty.Modulo-Financeiro/Modelo/FormaPagamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBeauty.Modulo_Financeiro.Modelo
+{
+    public class FormaPagamento
+    {
+        public int Id { get; private set; }
+        public string Tipo { get; private set; }
+
+
+        private FormaPagamento(int id)
+        {
+            Id = id;
+        }
+
+        public static FormaPagamento NovaFormaPagamento(int idFormaPagamento, string tipo)
+        {
+            if (! String.IsNullOrWhiteSpace(tipo))
+            {
+                var formaPagamento = new FormaPagamento(idFormaPagamento);
+                formaPagamento.Tipo = tipo;
+                return formaPagamento;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ TechBeauty/TechBeauty.Modulo-Financeiro/Modelo/Pagamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Modulo_Financeiro.Modelo
+{
+    public class Pagamento
+    {
+        public int Id { get; private set; }
+        public OrdemServico OS { get; private set; }
+        public FormaPagamento FormasPagamento { get; private set; }
+        public DateTime DataPagamento { get; private set; }
+
+        private Pagamento(int id)
+        {
+            Id = id;
+        }
+        public static Pagamento NovoPagamento(int idPagamento, OrdemServico os, FormaPagamento formasPagamento,
+         DateTime dataPagamento)
+        {
+            var novoPgt = new Pagamento(idPagamento);
+            novoPgt.OS = os;
+            novoPgt.FormasPagamento = formasPagamento;
+            novoPgt.DataPagamento = dataPagamento;
+            return novoPgt;
+        }
+    }
+}
