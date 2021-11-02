be9a55c0237c3ed1c13b839fd441a4f45fee616b
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-02 09:07:41 -0300

    Criando a Classe Contexto.

---
+++ TechBeauty/TechBeauty.Dados/Contexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TechBeauty.Dados.Map;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Dados
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<CargoContratoTrabalho> CargoContratoTrabalho { get; set; }
+        public DbSet<RegimeContratual> RegimeContratual { get; set; }
+        public DbSet<ContratoTrabalho> ContratoTrabalho { get; set; }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // User ID=carson; Password=123, caso seja autenticação usuário e senha do DB.
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9LK3UQP; Database=TechBeautyDB; Trusted_Connection=True"); //Trusted é para autenticação com usuário do Windows.
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CargoContratoTrabalhoMap());
+            modelBuilder.ApplyConfiguration(new RegimeContratualMap());
+            modelBuilder.ApplyConfiguration(new ContratoTrabalhoMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
