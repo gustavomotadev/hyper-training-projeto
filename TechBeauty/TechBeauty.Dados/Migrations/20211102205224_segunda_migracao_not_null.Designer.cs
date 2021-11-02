90e8631e19d3da8176e55643c1e1f9b18ba0d680
gsimoes00 <gustavomota001@gmail.com>
2021-11-02 17:54:00 -0300

    Segunda migracao

--- TechBeauty/TechBeauty.Dados/Migrations/20211102205224_segunda_migracao_not_null.Designer.cs
+++ TechBeauty/TechBeauty.Dados/Migrations/20211102205224_segunda_migracao_not_null.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechBeauty.Dados;
 
 namespace TechBeauty.Dados.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20211102205224_segunda_migracao_not_null")]
+    partial class segunda_migracao_not_null
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
