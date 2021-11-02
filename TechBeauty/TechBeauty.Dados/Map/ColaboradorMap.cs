f701bd1da95ff188b90734a5b6d8b271a743a1a8
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-02 11:15:06 -0300

    Adicionando ajustes do Entity na Classe ComtratoTrabalho.

---
+++ TechBeauty/TechBeauty.Dados/Map/ColaboradorMap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Dados.Map
+{
+    public class ColaboradorMap : IEntityTypeConfiguration<Colaborador>
+    {
+        public void Configure(EntityTypeBuilder<Colaborador> builder)
+        {
+            builder.ToTable("Colaborador");
+
+            builder.Property(x => x.NomeSocial)
+                .HasColumnType("varchar(100)");
+
+            builder.HasMany<ContratoTrabalho>
+                (c => c.Contratos)
+                .WithOne(c => c.Colaborador)
+                .HasForeignKey(c => c.ColaboradorId);
+
+            builder.HasMany<Turno>
+                (c => c.Turnos)
+                .WithOne(c => c.Colaborador)
+                .HasForeignKey(c => c.ColaboradorId);
+
+            builder.HasMany<Agendamento>
+             (c => c.Agendamentos)
+             .WithOne(c => c.Colaborador)
+             .HasForeignKey(c => c.ColaboradorId);
+
+        }
+    }
+}
