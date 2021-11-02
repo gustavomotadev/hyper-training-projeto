e0b09cd1d502563c9607349188820612722dd8d5
gsimoes00 <gustavomota001@gmail.com>
2021-11-02 16:36:14 -0300

    DbSets no contexto

--- TechBeauty/TechBeauty.Dados/Contexto.cs
+++ TechBeauty/TechBeauty.Dados/Contexto.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using TechBeauty.Dados.Map;
+using TechBeauty.Dominio.Financeiro;
 using TechBeauty.Dominio.Modelo;
 
 namespace TechBeauty.Dados
 {
     public class Contexto : DbContext
     {
+        public DbSet<Agendamento> Agendamento { get; set; }
+        public DbSet<CaixaDiario> CaixaDiario { get; set; }
+        public DbSet<Cargo> Cargo { get; set; }
         public DbSet<CargoContratoTrabalho> CargoContratoTrabalho { get; set; }
-        public DbSet<RegimeContratual> RegimeContratual { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Colaborador> Colaborador { get; set; }
+        public DbSet<ColaboradorServico> ColaboradorServico { get; set; }
+        public DbSet<Contato> Contato { get; set; }
         public DbSet<ContratoTrabalho> ContratoTrabalho { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Expediente> Expediente { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<OrdemServico> OrdemServico { get; set; }
+        public DbSet<PadraoRemuneracao> PadraoRemuneracao { get; set; }
+        public DbSet<Pagamento> Pagamento { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<RegimeContratual> RegimeContratual { get; set; }
+        public DbSet<RemuneracaoDiaria> RemuneracaoDiaria { get; set; }
+        public DbSet<RemuneracaoDiariaServico> RemuneracaoDiariaServico { get; set; }
+        public DbSet<Servico> Servico { get; set; }
+        public DbSet<TipoContato> TipoContato { get; set; }
+        public DbSet<Turno> Turno { get; set; }
 
-        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // User ID=carson; Password=123, caso seja autenticação usuário e senha do DB.
