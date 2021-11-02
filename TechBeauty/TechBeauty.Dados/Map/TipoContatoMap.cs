d008b09219a39c26d0220dcdfd33b22e22e410f7
gsimoes00 <gustavomota001@gmail.com>
2021-11-02 11:20:58 -0300

    Map de TipoContato implementado

---
+++ TechBeauty/TechBeauty.Dados/Map/TipoContatoMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechBeauty.Dominio.Modelo;
+
+namespace TechBeauty.Dados.Map
+{
+    class TipoContatoMap : IEntityTypeConfiguration<TipoContato>
+    {
+        public void Configure(EntityTypeBuilder<TipoContato> builder)
+        {
+            builder.ToTable("TipoContato");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Valor)
+                .HasColumnType("varchar(30)")
+                .IsRequired();
+
+            builder.HasMany<Contato>
+                (tc => tc.Contatos)
+                .WithOne(c => c.TipoContato)
+                .HasForeignKey(c => c.TipoContatoId);
+        }
+    }
+}
