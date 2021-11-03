4b2a6f9fbb83a85b25dda3eb5f6ac370c79d05b1
gsimoes00 <gustavomota001@gmail.com>
2021-11-03 17:37:36 -0300

    ViewModel Cliente alterado

--- TechBeauty/TechBeauty.API/ViewModels/CriarCliente.cs
+++ TechBeauty/TechBeauty.API/ViewModels/CriarCliente.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TechBeauty.API.ViewModels
 {
     public class CriarCliente
     {
+        //Dados de Pessoa
         [Required]
         public string Nome { get; set; }
         [Required]
         public string CPF { get; set; }
         [Required]
         public DateTime DataNascimento { get; set; }
+
+        //Contato obrigatório
         [Required]
-        public List<CriarContato> Contatos { get; set; } //TODO: viewmodel aninhado ?
+        public int TipoContato1Id { get; set; }
+        [Required]
+        public string Contato1 { get; set; }
+
+        //Contatos opcionais
+        public int TipoContato2Id { get; set; }
+        public string Contato2 { get; set; }
+        public int TipoContato3Id { get; set; }
+        public string Contato3 { get; set; }
     }
 }
