af6586fc204c8dd21cf0788fa42936b834f8d04c
gsimoes00 <gustavomota001@gmail.com>
2021-11-03 17:18:25 -0300

    ViewModel CriarAgendamento implementado

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarAgendamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarAgendamento
+    {
+        [Required]
+        public int ServicoId { get; set; }
+        [Required]
+        public int ColaboradorId { get; set; }
+        [Required]
+        public int OrdemServicoId { get; set; }
+        [Required]
+        public int ExpedienteId { get; set; }
+        [Required]
+        public string PessoaAtendida { get; set; }
+        [Required]
+        public DateTime DataHoraExecucao { get; set; }
+    }
+}
