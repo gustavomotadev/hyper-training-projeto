2b6a9b4b7c1668fcf09e586d842727537f4e4a2b
gsimoes00 <gustavomota001@gmail.com>
2021-11-03 17:20:34 -0300

    ViewModel Cargo implementado

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarCargo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarCargo
+    {
+        [Required]
+        public string Nome { get; private set; }
+        [Required]
+        public string Descricao { get; private set; }
+    }
+}
