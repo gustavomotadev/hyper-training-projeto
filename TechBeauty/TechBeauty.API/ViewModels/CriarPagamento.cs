b299eb54d5f9baec5a5daa28cfcc93225ad4eb75
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:46:54 -0300

    Criando a ViewModel CriarPagamento.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarPagamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarPagamento
+    {
+        [Required]
+        public DateTime DataPagamento { get; set; }
+        [Required]
+        public int OrdemServicoId { get; set; }
+        [Required]
+        public int FormaPagamentoId { get; set; }
+        [Required]
+        public int CaixaDiarioId { get; set; }
+        
+    }
+}
