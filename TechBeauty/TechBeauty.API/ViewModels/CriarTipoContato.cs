236f5b84587e76ad30861580660511dc492562f6
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 09:41:24 -0300

    Criando a ViewModel CriarTipoContato.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarTipoContato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarTipoContato
+    {
+        [Required]
+        public string Valor { get; set; }
+    }
+}
