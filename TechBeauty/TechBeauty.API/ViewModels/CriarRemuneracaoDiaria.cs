7aa686bfab35eb8947204200f50f56e3d47cb081
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:51:13 -0300

    Criando a ViewModel CriarRemuneracaoDiaria.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarRemuneracaoDiaria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarRemuneracaoDiaria
+    {
+        [Required]
+        public int ColaboradorId { get; set; }
+        [Required]
+        public int CaixaDiarioId { get; set; }
+        [Required]
+        public TimeSpan HorasTrabalhadas { get; set; }
+        [Required]
+        public List<int> ServicosRealizados { get; set; }
+
+    }
+}
