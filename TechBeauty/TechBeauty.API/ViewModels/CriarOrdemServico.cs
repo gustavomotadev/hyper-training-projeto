af71ae1e393e13a03a5e84d9d0b3b0bd898f7adc
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:29:35 -0300

    Criando a ViewModel CriarOrdemServico.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarOrdemServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarOrdemServico
+    {
+        [Required]
+        public int ClienteId { get; set; }
+        [Required]
+        public List<int> Agendamentos { get; set; }
+    }
+}
