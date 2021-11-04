0db549b30d69a8040df6d4400b2ceb3a044fb55e
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:06:02 -0300

    Criando a ViewModel CriarServico.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarServico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarServico
+    {
+        [Required]
+        public string Nome { get;  set; }
+        [Required]
+        public decimal Preco { get;  set; }
+        [Required]
+        public string Descricao { get;  set; }
+        [Required]
+        public int DuracaoEmMin { get;  set; }
+    }
+}
