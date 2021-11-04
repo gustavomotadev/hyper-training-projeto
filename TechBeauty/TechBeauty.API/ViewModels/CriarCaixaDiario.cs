eef6b0e4f650d8b919c7bc250535bd099fb7cf75
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:44:39 -0300

    Criando a ViewModel CriarCaixaDiario.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarCaixaDiario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarCaixaDiario
+    {
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public List<int> Pagamentos { get; set; }
+        [Required]
+        public List<int> Remuneracoes { get; set; }
+        
+        public static decimal CustoFixoPadrao { get; set; }
+
+
+    }
+}
