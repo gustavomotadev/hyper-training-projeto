049d6ec4ff983b29a54d74956ef425bbc05d164b
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:17:27 -0300

    Criando a ViewModel CriarExpediente.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarExpediente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarExpediente
+    {
+        [Required]
+        public DateTime DataHoraAbertura { get; set; }
+        [Required]
+        public DateTime DataHoraFechamento { get; set; 
+        
+    }
+}
