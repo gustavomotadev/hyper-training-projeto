42a7f6a23640d3ee07a7ca8d3684a7580060cf5c
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-04 10:23:22 -0300

    Criando a ViewModel CriarTurno.

---
+++ TechBeauty/TechBeauty.API/ViewModels/CriarTurno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBeauty.API.ViewModels
+{
+    public class CriarTurno
+    {
+        [Required]
+        public int ExpedienteId { get; set; }
+        [Required]
+        public int ColaboradorId { get; set; }
+        [Required]
+        public DateTime DataHoraEntrada { get; set; }
+        [Required]
+        public DateTime DataHoraSaida { get; set; }
+        
+        public DateTime? RegistroEntrada { get; set; }
+        public DateTime? RegistroSaida { get; set; }   
+    }
+}
