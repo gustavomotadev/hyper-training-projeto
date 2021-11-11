05ff9dca733d39ea21c48ab29b0c7670ea9b1371
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-11 13:06:05 -0300

    Criando a viewmodel de Alteração de Cargo.

---
+++ TechBeauty/TechBeauty.API/ViewModels/Alteracao/AlterarCargo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechBeauty.API.Interfaces;
+
+namespace TechBeauty.API.ViewModels.Alteracao
+{
+    public class AlterarCargo : IValidavel
+    {
+        public string Nome { get; set; }   
+        public string Descricao { get; set; }
+
+        public bool Validar()
+        {
+            return (!String.IsNullOrWhiteSpace(Nome) && !String.IsNullOrWhiteSpace(Descricao));
+        }
+    }
+}
