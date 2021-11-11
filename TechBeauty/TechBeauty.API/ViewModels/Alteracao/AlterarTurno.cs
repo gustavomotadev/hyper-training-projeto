65e748a2f26bcb096487372dcb688b0aa98936a7
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-11 13:00:34 -0300

    Criando a viewmodel de Alteração de Turno.

---
+++ TechBeauty/TechBeauty.API/ViewModels/Alteracao/AlterarTurno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechBeauty.API.Interfaces;
+
+namespace TechBeauty.API.ViewModels.Alteracao
+{
+    public class AlterarTurno : IValidavel
+    {
+        public DateTime DataHoraEntrada { get; set; }
+        
+        public DateTime DataHoraSaida { get; set; }
+
+        public bool Validar()
+        {
+            return (DataHoraEntrada.Date == DataHoraSaida.Date &&
+                DataHoraEntrada < DataHoraSaida);
+        }
+    }
+}
