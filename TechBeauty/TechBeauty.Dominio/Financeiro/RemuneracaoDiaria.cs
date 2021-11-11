7a9bf2e865fcc632b4655fef90bc44e11380efb9
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-11 11:59:15 -0300

    Private nas propriedades que estavam sem.

--- TechBeauty/TechBeauty.Dominio/Financeiro/RemuneracaoDiaria.cs
+++ TechBeauty/TechBeauty.Dominio/Financeiro/RemuneracaoDiaria.cs
@@ -8,11 +8,11 @@ namespace TechBeauty.Dominio.Financeiro
     public class RemuneracaoDiaria
     {
         public int Id { get; set; }
-        public int ColaboradorId { get; set; } //ef
+        public int ColaboradorId { get; private set; } //ef
         public Colaborador Colaborador { get; init; }
-        public List<Servico> Servicos { get; set; } //ef
-        public int CaixaDiarioId { get; set; } //ef
-        public CaixaDiario CaixaDiario { get; set; } //ef
+        public List<Servico> Servicos { get; private set; } //ef
+        public int CaixaDiarioId { get; private set; } //ef
+        public CaixaDiario CaixaDiario { get; private set; } //ef
         public TimeSpan HorasTrabalhadas { get; init; }
         public decimal ValorSalario { get; init; }
         public decimal ValorComissao { get; init; }
